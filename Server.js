34175c82be31c2af6bdfb1db2bbe6e66578cc370
Shubham Mishra <m.shubham0723@gmail.com>
2024-11-07 00:19:05 +0530

    updated server.js

--- Server.js
+++ Server.js
@@ -475,9 +475,12 @@ async function main() {
   });
   
 
-  app.listen(5000, function () {
-    console.log("Server started on port 5000");
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, function () {
+      console.log(`Server started on port ${port}`);
   });
+
 
 
 }
